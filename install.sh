da86cb86fce81f6bb94f6f77f98d454103a4bc95
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2024-01-01 19:58:35 +0200

    CLEANED UP SCRIPTS AND FILES
    -Organized into folders
    -made scripts us more of xargs instead of loops
    -made some important files copied instead of linked to prevent problems
    -removed thunderclient config

--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-#make hard links for dotfiles to the home directory
-for file in * .[^.]*; do
-	if [ -f "$file" ] && [ ${file:0:1} = "." ] && [  $file != ".gitignore" ] ; then  
-		if [ -f ../$file ]; then
-			rm ../$file
-		fi
-		ln -sf $PWD/$file ~/$file 
-	fi
-done
 
+shopt -s dotglob
+ln -sf $PWD/dotfiles/.* ~/
+
+ln -f $PWD/dotfiles/.gitconfig $PWD/
 
 #add font to system
 ##if the folders do not exist, make them
@@ -23,61 +18,51 @@ fi
 if [ ! -d $HOME/.local/share/fonts ] ; then
 	mkdir ~/.local/share/fonts
 fi
-cp JetBrainsMonoNerdFont-Regular.ttf ~/.local/share/fonts/JetBrainsMonoNerdFont-Regular.ttf
-
+ln -sf $PWD/fonts/* ~/.local/share/fonts/
 
 # kitty config
 if [ ! -L $HOME/.config/kitty ] ; then 
   rm -rf $HOME/.config/kitty  
 fi
+ln -sf $PWD/kitty ~/.config/
 
-ln -sf $(pwd)/kitty ~/.config/
+# ncurses music player c plus plus
+if [ ! -L $HOME/.config/ncmpcpp ] ; then 
+  rm -rf $HOME/.config/ncmpcpp  
+fi
+ln -sf $PWD/ncmpcpp ~/.config/
 
-# hard link the wallpaper image
-ln -f $(pwd)/wallpaper.jpg ~/
 
+# link the wallpaper image
+ln -sf $PWD/wallpaper.jpg ~/
 
+# zathura config
 if [ ! -L $HOME/.config/zathura ] ; then 
   rm -rf $HOME/.config/zathura
 fi
-# symbolically link my zathura config folder
-ln -sf $(pwd)/zathura ~/.config/
+ln -sf $PWD/zathura ~/.config/
 
 
 #VSCodium config
 ln -sf $PWD/VSCodium/settings.json ~/.config/VSCodium/User/settings.json
-
-extensions=$(cat $PWD/VSCodium/extensions)
-
-for extension_id in $extensions  
-do
-  echo $extension_id
-  codium --install-extension $extension_id
-done
+cat $PWD/VSCodium/extensions | xargs -I{} codium --install-extension {}
 
 
-if [ ! -L $HOME/.th-client ] ; then 
-  rm -rf $HOME/.th-client  
-fi
-ln -sf $(pwd)/.th-client ~/
-
+# simple x hotkey daemon config
 if [ ! -L $HOME/.config/sxhkd ] ; then 
   rm -rf $HOME/.config/sxhkd
 fi
-ln -sf $(pwd)/sxhkd ~/.config/
-
-# setting JAVA_HOME environment variable"
-echo "#!/bin/sh
-JAVA_HOME='/usr/lib/jvm/default'
-export JAVA_HOME"| sudo tee /etc/profile.d/javahome.sh 
+ln -sf $PWD/sxhkd ~/.config/
  
-#allows for sound
+#turns sound on 
 amixer sset Master unmute
 amixer sset Speaker unmute
 amixer sset Headphone unmute
 
 sudo usermod $USER -aG video  
 
+# make sure only root can access github oatuh token
 sudo chown root:root ~/.config/gh/hosts.yml
+
 echo awaiting root permissions for following steps
 sudo ./root_setup.sh
--- root_setup.sh
+++ root_setup.sh
@@ -3,48 +3,39 @@ if [ $(whoami) != 'root' ]; then
   exit 1
 fi
 
-cp -f .gitconfig /root/.gitconfig
 
-# this allows for shwoing a mouse inside a qemu vm
+shopt -s dotglob
+
+ls profile.d | xargs -I{} cp -f  profile.d/{} /etc/profile.d/{}
+
+cp -nf .gitconfig /root/.gitconfig
+
 if [ ! -d /etc/X11/xorg.conf.d ]; then
   mkdir /etc/X11/xorg.conf.d
 fi
-echo "putting mouse config"
-ln -sf $(pwd)/vesa-swcursor.conf /etc/X11/xorg.conf.d/
-
-#link doas config
-ln -sf $(pwd)/doas.conf /etc/doas.conf
-chown root:root doas.conf
-chmod 644 doas.conf
-# link my screenshot scrot script
-
-ln -sf $(pwd)/slim.conf /etc/slim.conf
-chown root:root slim.conf
-chmod 664 slim.conf
+ls xorg.conf.d | xargs -I{} cp -f  xorg.conf.d/{} /etc/X11/xorg.conf.d/{}
 
 
-for file in $(pwd)/programs/*
-do
-  cp -f $file /usr/bin/
-  chmod 775 $file
-done
+cp -f $PWD/slim/slim.conf /etc/slim.conf
+chown root:root slim/slim.conf
+chmod 664 slim/slim.conf
 
+ls programs | xargs -I{} cp -f programs/{} /usr/bin/{}
 
-ln -sf $PWD/sudoers /etc/sudoers
-chown root:root sudoers
+cp -f $PWD/sudo/sudoers /etc/sudoers
+chown root:root sudo/sudoers
 
 # change owner of these important files to root
 chown root:root programs/lock 
-
 chown root:root programs/locker
 
-ln -sf $(pwd)/picom.conf /etc/xdg/picom.conf
+cp -f $PWD/picom/picom.conf /etc/xdg/picom.conf
 
-ln -sf $(pwd)/customLogin /usr/share/slim/themes/
-chmod -R 645 customLogin
-chown -R root:root customLogin
+cp -Rf $PWD/slim/customLogin /usr/share/slim/themes/
+chmod -R 645 slim/customLogin
+chown -R root:root slim/customLogin
 
-ln -sf  backlight.rules /etc/udev/rules.d/
+ls udevrules | xargs -I{} cp udevrules/{} /etc/udev/rules.d/{}
 
 # removes vi and vim to instead use neovim (https://neovim.io/)
 rm /usr/bin/vi
