d59c2d0532a877550af00d2fe40566fb3fd9f797
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2024-01-28 04:44:27 +0200

    Configured hyprland
    -configured waybar and wayidle
    -still problems with temp and cpu usage with vids
    -got workspaces

--- install.sh
+++ install.sh
@@ -59,6 +59,15 @@ if [ ! -L ~/.config/zathura ] ; then
 fi
 ln -sf $PWD/zathura ~/.config/
 
+if [ ! -L ~/.swaylock ] ; then 
+  rm -rf ~/.swaylock
+fi
+ln -sf $PWD/.swaylock ~/.swaylock
+
+if [ ! -L ~/.swayidle ] ; then 
+  rm -rf ~/.swayidle
+fi
+ln -sf $PWD/.swayidle ~/.swayidle
 
 #VSCodium config
 ln -sf $PWD/VSCodium/settings.json ~/.config/VSCodium/User/settings.json
