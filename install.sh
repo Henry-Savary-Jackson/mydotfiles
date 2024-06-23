7e2fc7d987565f966dab17c5d1cb16fe0694312a
Henry Savary-Jackson <96239862+Henry-Savary-Jackson@users.noreply.github.com>
2024-06-23 04:38:22 +0200

    Changes!
    Added a for loop in the install.sh to add all files in .config directory
    add btop and htop config
    added on modified neofetch config

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 shopt -s dotglob
+
 ln -sf $PWD/dotfiles/.* ~/
 
 ln -f $PWD/dotfiles/.gitconfig $PWD/
@@ -21,60 +22,32 @@ if [ ! -d ~/.local/share/fonts ] ; then
 fi
 ln -sf $PWD/fonts/* ~/.local/share/fonts/
 
-# kitty config
-if [ ! -L ~/.config/kitty ] ; then 
-  rm -rf ~/.config/kitty  
-fi
-ln -sf $PWD/kitty ~/.config/
+config_dirs=( $(ls .config ) )
+for directory in ${config_dirs[@]} 
+do
+  if [[ -d ~/.config/$directory && ! -L ~/.config/$directory  ]] ; then 
+    rm -rf ~/.config/$directory
+  else
+    rm ~/.config/$directory
+  fi
+  ln -sf $PWD/.config/$directory ~/.config/$directory
+done
 
-# kitty config
-if [ ! -L ~/.config/rofi ] ; then 
-  rm -rf ~/.config/rofi  
-fi
-ln -sf $PWD/rofi ~/.config/
 
-# music player per user configuration
-if [ ! -L  ~/.config/mpd ] ; then
-  rm -rf ~/.config/mpd
-fi
-ln -sf $PWD/mpd ~/.config/
+#VSCodium config
+cat $PWD/.config/VSCodium/extensions | xargs -I{} codium --install-extension {}
 
-# ncurses music player c plus plus
-if [ ! -L  ~/.config/ncmpcpp ] ; then 
-  rm -rf ~/.config/ncmpcpp  
-fi
-ln -sf $PWD/ncmpcpp ~/.config/
-
+# add pre-commit hook to add extensions
+ln -sf $PWD/.config/VSCodium/git-save-vscodium-extension-precomm-hook $PWD/.git/hooks/
 
 # link the wallpaper image
 ln -sf $PWD/wallpaper.jpg ~/
 
-# zathura config
-if [ ! -L ~/.config/zathura ] ; then 
-  rm -rf ~/.config/zathura
-fi
-ln -sf $PWD/zathura ~/.config/
-
-
-#VSCodium config
-ln -sf $PWD/VSCodium/settings.json ~/.config/VSCodium/User/settings.json
-cat $PWD/VSCodium/extensions | xargs -I{} codium --install-extension {}
-
-# add pre-commit hook to add extensions
-ln -sf $PWD/VSCodium/git-save-vscodium-extension-precomm-hook $PWD/.git/hooks/
-
-# simple x hotkey daemon config
-if [ ! -L ~/.config/sxhkd ] ; then 
-  rm -rf ~/.config/sxhkd
-fi
-ln -sf $PWD/sxhkd ~/.config/
- 
 #turns sound on 
-amixer sset Master unmute
+amixer sset Master unmute 
 amixer sset Speaker unmute
 amixer sset Headphone unmute
 
-sudo usermod $USER -aG video  
-
 echo awaiting root permissions for following steps
 sudo ./root_setup.sh
+sudo usermod $USER -aG video  
